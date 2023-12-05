168fce7336d269a85d02c690eb9ec074afc81d6d
ANTONIO VINÍCIUS <146228058+ViniciusOliver13@users.noreply.github.com>
2023-12-05 20:03:54 -0300

    Adição dos comentários Dia-05-12-23-ex_2.c

--- Aulas/Dia-05-12-23-ex_2.c
+++ Aulas/Dia-05-12-23-ex_2.c
@@ -7,16 +7,16 @@
 int main(){
 
     int tamanho = 5, i;
-    int *vetor = (int *)malloc(tamanho * sizeof(int));
+    int *vetor = (int *)malloc(tamanho * sizeof(int)); //Alocando o vetor com 5 posições.
 
     if (vetor == NULL){
-        exit(0);
+        exit(0);       //Função "exit()" para finalizar a execução do código.
     }else{
         printf("ALOCACAO realizada com sucesso\n");
     }
 
     for ( i = 0; i < tamanho; i++){
-        printf("Digite os valores do vetor: ");
+        printf("Digite os valores do vetor: ");        //Preenchedo o vetor com números inteiros.
         scanf("%d", vetor + i);
     }
 
