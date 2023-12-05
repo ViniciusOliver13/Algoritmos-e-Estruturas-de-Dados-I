ebb90291f27a180124b3de9ea25a5c03d07bf4bd
ANTONIO VINÍCIUS <146228058+ViniciusOliver13@users.noreply.github.com>
2023-12-04 21:13:19 -0300

    Melhorando o código Dia-04-12-2023.c
    Foram implementados algumas mudanças, mas a essência do conteúdo trabalhado em sala de aula foi preservado.

--- Aulas/Dia-04-12-2023.c
+++ Aulas/Dia-04-12-2023.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 int soma(int a, int b){
 
@@ -16,7 +17,7 @@ int multiplicacao(int a, int b){
     return a * b;
 }
 
-int divicao(int a, int b){
+int divicaoInteira(int a, int b){
 
     return a / b;
 }
@@ -26,36 +27,59 @@ int calcula(int x, int y, int (*operacao)(int, int)){
     return (*operacao)(x,y);
 }
 
+void numeros(int *n1, int *n2){
+    printf("Digite o 1º valor: ");
+    scanf("%d", n1);
+    printf("Digite o 2º valor: ");
+    scanf("%d", n2);
+}
 int main(){
+    setlocale(LC_ALL,"Portuguese");
 
-    // PONTEIROS PARA FUNÇÕES
-    int escolha, resultado;
+    // ASSUNTO: *** PONTEIROS PARA FUNÇÕES ***
+    int escolha, resultado, num1, num2;
 
-    printf("DIGITE A OPCAO:");
-    scanf("%d", &escolha);
+    do{
+        printf("----------------------\n[1] SOMA\n[2] SUBTRAÇÃO\n[3] MULTIPLICAÇÃO\n[4]" 
+        " DIVISÃO INTEIRA\n[5] SAIR\n----------------------\n");
+        printf("DIGITE UMA OPÇÃO: ");
+        scanf("%d", &escolha); 
 
-    switch (escolha)
-    {
-    case 1:
-        resultado = calcula(5, 3, soma);
-        printf("Resultado: %d", resultado);
-        break;
-    case 2:
-        resultado = calcula(5, 3, subtracao);
-        printf("Resultado: %d", resultado);
-        break;
-    case 3:
-        resultado = calcula(5, 3, multiplicacao);
-        printf("Resultado: %d", resultado);
-        break;
-    case 4:
-        resultado = calcula(5, 2, divicao);
-        printf("Resultado: %d", resultado);
-        break;
-    default:
-        printf("opcao invalida!");
-        break;
-    }
+        //numeros(&num1, &num2); --- >>> CHAMADA PARA O PROCEDIMENTO DE PREENCHER OS 2 NÚMEROS
 
+        switch (escolha){
+            case 1:
+                numeros(&num1, &num2);
+                resultado = calcula(num1, num2, soma);
+                printf("Resultado da SOMA: %d\n", resultado);
+                break;
+            case 2:
+                numeros(&num1, &num2);
+                resultado = calcula(num1, num2, subtracao);
+                printf("Resultado da SUBTRAÇÃO: %d\n", resultado);
+                break;
+            case 3:
+                numeros(&num1, &num2);
+                resultado = calcula(num1, num2, multiplicacao);
+                printf("Resultado da MULTIPLIÇÃO: %d\n", resultado);
+                break;
+            case 4:
+                numeros(&num1, &num2);
+                resultado = calcula(num1, num2, divicaoInteira);
+                printf("Resultado da DIVISÃO INTEIRA: %d\n", resultado);
+                break;
+            case 5:
+                printf("Saindo...");
+                exit(0);
+            default:
+                printf("opção inválida!\n");
+                break;
+        }
+
+    system("pause");
+    system("cls");
+    
+    }while(escolha != 5);
+    
     return 0;
 }
