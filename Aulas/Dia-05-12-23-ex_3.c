f0555908fe867f3ade0b658afecf4fae53428082
ANTONIO VINÍCIUS <146228058+ViniciusOliver13@users.noreply.github.com>
2023-12-05 19:23:20 -0300

    Adição dos comentários Dia-05-12-23-ex_3.c

--- Aulas/Dia-05-12-23-ex_3.c
+++ Aulas/Dia-05-12-23-ex_3.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* ALOCAÇÃO DINÂMICA  
-
-*/
+/* ALOCAÇÃO DINÂMICA COM REALLOC 
+*/    
 int main(){
 
     int tamanho = 5, novo_tamanho;
 
-    int *vetor = (int *)malloc(tamanho * sizeof(int));
-
-    fflush(stdin);
+    int *vetor = (int *)malloc(tamanho * sizeof(int));       //Alocando o vetor com 5 posições.
+    
     printf("\nEntre com o novo tamanho do vetor: ");
-    scanf("%d", &novo_tamanho);
+    scanf("%d", &novo_tamanho);                              //Informando o novo tamanho do vetor.  
 
-    vetor = (int *) realloc (vetor, novo_tamanho * sizeof(int));
+    vetor = (int *) realloc (vetor, novo_tamanho * sizeof(int));    //Fazendo a realocação com a função "realloc".
 
     if (vetor == NULL){
         printf("\nErro na alocacao!");
