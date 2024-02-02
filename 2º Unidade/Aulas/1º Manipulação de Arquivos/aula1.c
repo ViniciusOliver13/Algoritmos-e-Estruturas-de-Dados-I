5a7d47a473c920090c5f68004240ca2001c237fb
Antonio Vinícius <146228058+ViniciusOliver13@users.noreply.github.com>
2024-02-02 20:11:00 -0300

    Correção

--- 2º Unidade/Aulas/1º Manipulação de Arquivos/aula1.c
+++ 2º Unidade/Aulas/1º Manipulação de Arquivos/aula1.c
@@ -3,33 +3,24 @@
 
 int main(){
 
-    FILE *arquivo_entrada = fopen("entrada_3q.txt", "r");
-    FILE *arquivo_saida = fopen("sainda_3q.txt", "wt");
-    int i, tam;
+    FILE *arquivo_entrada = fopen("entrada_3q.txt", "r");     //Abrindo o arquivo entrada no modo de leitura.
+    FILE *arquivo_saida = fopen("saida_3q.txt", "wt");      // Abrindo o arquivo saida no modo de escrita de texto.  
+
     char nome[50];
     float n1, n2, n3, media = 0;
 
-    if(arquivo_entrada == NULL){
-        printf("Erro ao abrir o arquivo de entrada!");
-        exit(1);
-    }
-
-    while(sscanf(arquivo_entrada, "%20[^\t]\t%f\t%f\t%f", nome, &n1, &n2, &n3) != 1){
-        media = (n1 + n2 + n3) / 3 ;
-        fprintf(arquivo_saida, "%s\t%.2f\t%s\n", nome, media, media >= 7.0 ? "Aprovado" : "Reprovado");
+     while(fscanf(arquivo_entrada, "%s\t%f\t%f\t%f\n", nome, &n1, &n2, &n3) == 4){      //Lendo o conteúdo do arquivo de entrada com a função "fscanf()".
+        media = (n1 + n2 + n3) / 3;
+        fprintf(arquivo_saida, "%s\t\tMEDIA: %.2f\t\t%s\n", nome, media, media >= 7.0 ? "Aprovado" : "Reprovado");      //Adicionando as informações ao arquivo de saida com a função "fprinf()".
         media = 0.0;
     }
-    
-    if(arquivo_saida == NULL){
-        printf("Erro ao abrir o arquivo de saida!");
-        exit(1);
-    }    
     
     if(fclose(arquivo_entrada) == 0){
         printf("Arquivo fechado!");
     }
-    if(fclose(arquivo_saida)){
-        printf("Arquivo fechado");
+
+    if(fclose(arquivo_saida) == 0){
+        printf("Arquivo modificado!");
     }
 
     return 0;
