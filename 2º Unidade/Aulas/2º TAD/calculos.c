d36e57d7b0b2430abd4356a4b94c939d3168d6ea
Antonio Vinícius <146228058+ViniciusOliver13@users.noreply.github.com>
2024-02-04 12:14:33 -0300

    Add files via upload

---
+++ 2º Unidade/Aulas/2º TAD/calculos.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "funcoes.h"        //Arquivo onde é realizado os cálculos das funções.
+
+int adicao(int a1, int a2){
+    int res;
+    res = a1 + a2;
+
+    return res;
+}
+
+int subtracao(int a1, int a2){
+    int res;
+    res = a1 - a2;
+
+    return res;
+}
+
+int multiplicacao(int a1, int a2){
+    int res;
+    res = a1 * a2;
+
+    return res;
+}
+
+float divisao(int a1, int a2){
+    float res;
+    res = a1 / a2;
+    
+    return res;
+}
+
+int potenciacao(int a1, int a2){
+    int i, res = 1;
+    res = a1;
+    for(i = 1; i < a2; i++){
+        res *= a1;
+    }
+    
+    return res;
+}
