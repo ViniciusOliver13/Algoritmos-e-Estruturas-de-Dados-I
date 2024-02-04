fc8703a87dc8fd744d890a111a906f8565c0728b
Antonio Vinícius <146228058+ViniciusOliver13@users.noreply.github.com>
2024-02-04 12:18:37 -0300

    Add files via upload

---
+++ 2º Unidade/Aulas/2º TAD/funcoes.h
@@ -0,0 +1,14 @@
+//Realiza a adição entre dois números inteiros.
+int adicao(int n1, int n2);
+
+//Realiza a subtração entre dois números inteiros.
+int subtracao(int n1, int n2);
+
+//Realiza a multiplicação entre dois números inteiros.
+int multiplicacao(int n1, int n2);
+
+//Realiza a divisão entre dois números inteiros.
+float divisao(int n1, int n2);
+
+//Realiza a potenciação de um número.
+int potenciacao(int n1, int n2);
