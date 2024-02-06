8c9a456f2d93fdc2d7b2b388bac74dc2e8999dd8
Antonio Vinícius <146228058+ViniciusOliver13@users.noreply.github.com>
2024-02-06 11:02:22 -0300

    Update main.c

--- 2º Unidade/Aulas/2º TAD/main.c
+++ 2º Unidade/Aulas/2º TAD/main.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h> 
-#include "aluno.h"
+#include "aluno.c"
 /* TAD : Tipo Abstrato de Dados */
-/* TAD : um novo tipo (struct) + cojunto de operações */
+/* TAD : um novo tipo (struct) + cojunto de operaÃ§Ãµes */
 
 int main(){
 
-    Aluno *aluno = NULL;
-    int tamanho = 0;
-
     printf("\033[1mCastrado de alunos\n=-=-=-=-=-=-=-=-=-=-=-=\n");
-    printf("Qual a quantidade de alunos? ");
-    scanf("%d", &tamanho);
 
-    aluno = matricular_alunos(tamanho);
-    
-    imprimir(aluno, tamanho);
+    Aluno *aluno = matricular_alunos();
 
     liberar(aluno);
 
