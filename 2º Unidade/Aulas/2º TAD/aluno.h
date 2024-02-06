e93f41679d378e492614b43efb69a0526b898f3f
Antonio Vinícius <146228058+ViniciusOliver13@users.noreply.github.com>
2024-02-05 21:08:55 -0300

    Aula dia 05/02/24

---
+++ 2º Unidade/Aulas/2º TAD/aluno.h
@@ -0,0 +1,15 @@
+//Definição de novo tipo: Aluno
+typedef struct aluno Aluno;
+
+//Função que aloca memória para uma struct aluno, recebe via teclado 
+//e retorna um ponteiro para Aluno.
+Aluno *recebe_dados(void);
+
+//Função que imprime os dado(s) dos aluno(s).
+void imprimir(Aluno *ponteiro, int tamanho);
+
+//Função que libera memória ao fim da execução.
+void liberar(Aluno *ponteiro);
+
+//Função que matricula um determinado número de alunos.
+Aluno *matricular_alunos(int max);
